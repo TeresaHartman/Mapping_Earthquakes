e08789d7dbabfc5ee02dcc8b2df1bb923c680b51
Teresa Hartman <teresa2h@users.campus>
2022-09-28 21:58:25 -0500

    Updting logic.js to including mapping single point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // // An althernate to the code above. Putting here for reference later
 // // Create the map object with a center and zoom level.
@@ -13,11 +13,20 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+//  Add a marker to the map for Los Angeles, California.
+    //let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Change marker to a circle
+L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color: "black", 
+    fillColor: "#ffffa1"
+ }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    //id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     // To change the map's style, change the map id using the list of Mapbox ids below:
         // mapbox/streets-v11
         // mapbox/outdoors-v11
