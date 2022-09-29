3e846eb89d187b73c2aa7532fbcd00c3b1ceb0d1
Teresa Hartman <teresa2h@users.campus>
2022-09-28 22:09:44 -0500

    Including files for mapping lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
 let cityData = cities;
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6, -110.4], 5);
 
 // // An althernate to the code above. Putting here for reference later
 // // Create the map object with a center and zoom level.
@@ -16,26 +16,26 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
 
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city);
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        color: "orange",
-        weight: "4",
-        fillColor: "#ffffa1"
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-   });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    //id: 'mapbox/streets-v11',
+    id: 'mapbox/satellite-streets-v11',
     // To change the map's style, change the map id using the list of Mapbox ids below:
         // mapbox/streets-v11
         // mapbox/outdoors-v11
