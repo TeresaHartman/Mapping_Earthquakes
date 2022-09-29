2fbf471257b5ab42eeb0d5ae63da033239f2548e
Teresa Hartman <teresa2h@users.campus>
2022-09-29 14:25:58 -0500

    Updating Tornoto neighborhoods

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -6,10 +6,10 @@ console.log("working");
 // let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let dayN = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/navigation-preview-day-v4',
+    id: 'mapbox/streets-v11',
     // To change the map's style, change the map id using the list of Mapbox ids below:
         // mapbox/streets-v11
         // mapbox/outdoors-v11
@@ -21,45 +21,57 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let nightN = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id:'mapbox/satellite-streets-v11',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    "Day Navigation": dayN,
-    "Night Navigation": nightN
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [30, 30],
-    zoom: 2,
-    layers: [nightN]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
   });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-
-// // Accessing the airport GeoJSON URL
-// let airportData = "https://raw.githubusercontent.com/TeresaHartman/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON/majorAirports.json";
-
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/TeresaHartman/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/TeresaHartman/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-    console.log(data);
+d3.json(torontoHoods).then(function(data) {
+     console.log(data);
+     //Creating a GeoJSON layer with the retrieved data.
+     //L.geoJSON(data).addTo(map);
 
     L.geoJson(data,{
-        "color": "#FAF884", 
-        "weight": 2,
+        "color": "#0000FF", 
+        "weight": 1,
         onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " +
-                     feature.properties.dst + "</h3>");
+        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
         }
     }).addTo(map);
 });
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
+
+//     L.geoJson(data,{
+//         "color": "#FAF884", 
+//         "weight": 2,
+//         onEachFeature: function(feature, layer) {
+//         layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " +
+//                      feature.properties.dst + "</h3>");
+//         }
+//     }).addTo(map);
+// });
