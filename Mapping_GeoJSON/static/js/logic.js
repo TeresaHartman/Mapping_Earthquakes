463c9a137b8a6c32458d558060d051e47e4fbace
Teresa Hartman <teresa2h@users.campus>
2022-09-28 22:16:01 -0500

    Including files for Mapping GeoJSON

--- Mapping_GeoJSON/static/js/logic.js
+++ Mapping_GeoJSON/static/js/logic.js
@@ -1,11 +1,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Get data from cities.js
-let cityData = cities;
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6, -110.4], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // // An althernate to the code above. Putting here for reference later
 // // Create the map object with a center and zoom level.
@@ -16,26 +14,43 @@ let map = L.map('mapid').setView([37.6, -110.4], 5);
 //     zoom: 4
 //   });
 
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-}).addTo(map);
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+      .bindPopup("<h2>" + feature.properties.city + "</h2>")
+    }
+
+  }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/streets-v11',
     // To change the map's style, change the map id using the list of Mapbox ids below:
         // mapbox/streets-v11
         // mapbox/outdoors-v11
